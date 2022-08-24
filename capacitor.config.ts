319f9267bc7f76722b9e4ba3b9be080e1e1a9951
Mohammad-Al-Najar <44784702+mohammadalnajar@users.noreply.github.com>
2022-08-24 16:21:53 +0200

    build android app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ionic-project',
-  webDir: 'build',
-  bundledWebRuntime: false
+    appId: 'first-ionic-app-mo-97',
+    appName: 'ionic-project',
+    webDir: 'build',
+    bundledWebRuntime: false
 };
 
 export default config;
